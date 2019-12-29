d86669ca978652a62c51f3f945862f2174bcd7fa
rrrochaa <silva.rodrigo31@outlook.com>
2019-12-28 23:11:54 -0200

    final

--- regressao/app.R
+++ regressao/app.R
@@ -1,38 +1,45 @@
-library(shiny)
-
-
-ui <- fluidPage(
-    titlePanel("Previsão de Custo Inicial para Montar a Franquia"),
-    fluidRow(
-        column(4,
-            h2("Dados"),
-            tableOutput("Dados")
-            
-        ),
-        column(8,
-            plotOutput("Graf")
-            
-        )
-    ),
-    fluidRow(
-        column(6,
-           h3("Valor Anual da Franquia: "),
-           numericInput("NovoValor", "Insira novo valor", 1500, min = 1, max = 99999999),
-           actionButton("Processar", "Processar")
-        ),
-        column(6,
-           h1(textOutput("Resultado"))
-        )
-    )   
-)
-    
-
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+                    library(shiny)
+                    
+                    dados = read.csv("slr12.csv", sep = ";")
+                    
+                    modelo = lm(CusInic ~ FrqAnual, data = dados)
+                    
+                    ui <- fluidPage(
+                        titlePanel("Previsão de Custo Inicial para Montar a Franquia"),
+                        fluidRow(
+                            column(4,h2("Dados"), tableOutput("Dados")),
+                            column(8, plotOutput("Graf"))
+                            ),
+                        fluidRow(
+                            column(6,
+                                   h3("Valor Anual da Franquia: "),
+                                   numericInput("NovoValor", "Insira novo valor", 1500, min = 1, max = 99999999),
+                                   actionButton("Processar", "Processar")
+                            ),
+                            column(6,
+                                   h1(textOutput("Resultado"))
+                            )
+                        )   
+                    )
+                    
+                    
+                    server <- function(input, output) {
+                        output$Graf = renderPlot({
+                                plot(CusInic ~ FrqAnual, data = dados)
+                                    abline(modelo)
+                        })
+                        
+                            output$Dados = renderTable({head(dados, 10)})
+                        
+                        observeEvent(input$Processar, {
+                            valr = input$NovoValor
+                            prev = predict(modelo, data.frame(FrqAnual = eval(parse(text = valr))))
+                            prev = paste0("Previsão de custo Inicial R$: ", round(prev, 2))
+                            output$Resultado = renderText({prev})
+                        })
+                    }
+                    
+                    
+                    # Run the application 
+                    shinyApp(ui = ui, server = server)
+                    
